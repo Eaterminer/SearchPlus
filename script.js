a3699871246ffdb1e79dee289167ff5b230df981
Eaterminer <58890567+Eaterminer@users.noreply.github.com>
2021-03-24 14:07:39 +1300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 function saveBg() {
     bg = document.getElementById("bg").value;
     localStorage.setItem("bg", bg);
+    document.getElementById("body").style.background = bg;
 }
 function saveColor() {
-    bg = document.getElementById("color").value;
+    color = document.getElementById("color").value;
     localStorage.setItem("color", color);
+    document.getElementById("body").style.color = color;
 }
 function saveEngine() {
     engine = document.getElementById("engine").value;
     localStorage.setItem("engine", engine);
 }
+function search() {
+    searchTerm = document.getElementById("search").value;
+    link = "https://google.com/search?q=" + searchTerm;
+    window.open(link);
+}
+color = localStorage.getItem("color");
+bg = localStorage.getItem("bg");
+engine = localStorage.getItem("engine");
+document.getElementById("body").style.color = color;
+document.getElementById("body").style.color = "#dcdcdc";
+document.getElementById("body").style.background = bg;
